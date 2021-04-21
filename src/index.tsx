f691950b2cb620e80d02899a24587f7a081dbf17
Morgan Spencer <morgan@spencercreative.co>
2021-04-21 14:51:28 -0400

    Structure src

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,45 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import styles from './styles.module.scss'
+import classNames from 'classnames'
 
 interface Props {
-  text: string
+  brand: string
+  className: string
+  variant: string | string[]
+  as: string
+  [x: string]: any
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = (props: Props) => {
+  const { brand, className, variant, as, ...rest } = props
+
+  var variants: string[] = []
+
+  function addVariant(name: string) {
+    variants.push(styles[`${brand}-${name}`])
+  }
+
+  if (variant && Array.isArray(variant)) {
+    variant.forEach((element) => {
+      addVariant(element)
+    })
+  } else if (typeof variant === 'string') {
+    addVariant(variant)
+  }
+
+  const componentClassNames = classNames(
+    brand && styles[brand],
+    brand && variants.length > 0 && variants,
+    className
+  )
+
+  console.log(as)
+
+  const CustomTag = as || 'button'
+
+  return (
+    <CustomTag className={componentClassNames} {...rest}>
+      {props.children}
+    </CustomTag>
+  )
 }
