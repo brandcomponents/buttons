aea8568cac797bcb00c3ed56213a76a02a5cabe5
Morgan Spencer <morgan@spencercreative.co>
2021-04-21 14:51:37 -0400

    Update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from '@brandcomponents/buttons'
+import { Button } from '@brandcomponents/buttons'
 import '@brandcomponents/buttons/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button brand="facebook" className="testing-cn">Facebook</Button>
+      <Button type="button" brand="facebook" variant={['light', 'wide']} className="testing-cn">Facebook</Button>
+      <Button brand="facebook" variant="gray" className="testing-cn">Facebook</Button>
+    </>
+  )
 }
 
 export default App
